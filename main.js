7d706f71e488e5d25eb5174a68798321278c93a8
Kai Paterson - Hall <kaizwebdesign@gmail.com>
2020-09-05 13:09:16 +0100

    Add module to store DOM elements

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 const gameBoard = (() => {
     let gameBoardArray = [
-        ['X', 'X', 'X'],
-        ['X', 'O', 'X'],
-        ['X', 'X', 'O']
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
     ];
 
     return { gameBoardArray };
 })();
 
-const displayController = (() => {
-    // get DOM elements
-    const gameBoardElement = document.querySelector('.gameboard');
-    const gameBoardCells = gameBoardElement.querySelectorAll('div');
+const domElements = (() => {
+        const _gameBoardElem = document.querySelector('.gameboard');
 
+        const gameBoardCells = [..._gameBoardElem.querySelectorAll('div')];
+
+    return { gameBoardCells }
+})();
+
+const displayController = (() => {
     // render gameboard array to screen
     const render = () => {
-        gameBoardCells.forEach(cell => {
+        domElements.gameBoardCells.forEach(cell => {
             cell.textContent =
                 gameBoard.gameBoardArray[cell.dataset.row][cell.dataset.column];
         });
